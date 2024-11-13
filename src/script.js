49299898be61d01d8b2ce9c0c2c2ff4f9587199c
hugomonntes <huugomontees@gmail.com>
2024-11-13 12:30:22 +0100

    Update

--- src/script.js
+++ src/script.js
@@ -1,56 +1,10 @@
-const btnLeft = document.querySelector(".btn-left"),
-    btnRight = document.querySelector(".btn-right"),
-    slider = document.querySelector("#slider"),
-    sliderSection = document.querySelectorAll(".slider-section");
-
-btnLeft.addEventListener("click", e => moveToLeft())
-btnRight.addEventListener("click", e => moveToRight())
-
-setInterval(() => {
-    moveToRight()
-}, 3500);
-
-let operacion = 0,
-    counter = 0,
-    widthImg = 100 / sliderSection.length;
-
-function moveToRight() {
-    if (counter >= sliderSection.length-1) {
-        counter = 0;
-        operacion = 0;
-        slider.style.transform = `translate(-${operacion}%)`;
-        slider.style.transition = "none";
-        return;
-    } 
-    counter++;
-    operacion = operacion + widthImg;
-    slider.style.transform = `translate(-${operacion}%)`;
-    slider.style.transition = "all ease .6s"
-    
-}  
-
-function moveToLeft() {
-    counter--;
-    if (counter < 0 ) {
-        counter = sliderSection.length-1;
-        operacion = widthImg * (sliderSection.length-1)
-        slider.style.transform = `translate(-${operacion}%)`;
-        slider.style.transition = "none";
-        return;
-    } 
-    operacion = operacion - widthImg;
-    slider.style.transform = `translate(-${operacion}%)`;
-    slider.style.transition = "all ease .8s"
-}
-
 let titulo = document.getElementById('tituloReloj');
+let fechaLimite = new Date()
 addEventListener('load', mostrarReloj);
 setInterval(mostrarReloj, 1000);
-let dias = ['DOMINGO', 'LUNES', 'MARTES', 'MIERCOLES', 'JUEVES', 'VIERNES', 'SÁBADO'];
 function mostrarReloj(){
     let fechaActual = new Date()
     let dia = fechaActual.getDay()
-    let posicionDia = dias[dia];
     let fechaExacta = fechaActual.toLocaleTimeString();
-    titulo.innerHTML = fechaExacta + ' ' +posicionDia;
+    titulo.innerHTML = fechaExacta;
 }
