7179f87ab67d44589c0ad9e4964d8c640d7e1da2
Hugo Montes <huugomontees@gmail.com>
2024-11-13 20:13:06 +0100

    Update

--- src/script.js
+++ src/script.js
@@ -1,10 +1,16 @@
-let titulo = document.getElementById('tituloReloj');
-let fechaLimite = new Date()
-addEventListener('load', mostrarReloj);
-setInterval(mostrarReloj, 1000);
-function mostrarReloj(){
-    let fechaActual = new Date()
-    let dia = fechaActual.getDay()
-    let fechaExacta = fechaActual.toLocaleTimeString();
-    titulo.innerHTML = fechaExacta;
-}
+const targetDate = new Date(2025, 2, 14).getTime();;
+
+        function updateCountdown() {
+            const now = new Date().getTime();
+            const timeLeft = targetDate - now;
+
+            const days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
+            const hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+            const minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
+            const seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
+
+            document.getElementById("countdown").innerHTML = 
+                `${days} DAYS ${hours} HOURS ${minutes} MINUTS ${seconds} SECONDS`;
+        }
+
+        setInterval(updateCountdown, 1000);
